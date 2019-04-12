55a73b254d3fb295c080cde7dd373e0a5ac6616f
Jan Unterbrink <jan.unterbrink@inovex.de>
2019-04-12 11:04:51 +0200

    not using static ip address `

--- main.sh
+++ main.sh
@@ -16,11 +16,11 @@ id=$1
 ip=$2
 
 
-mosquitto_pub -h 192.168.99.1 -t "$hw/events/device/$id/state/update" -m '{"state":"online"}'
+mosquitto_pub -h $ip -t "$hw/events/device/$id/state/update" -m '{"state":"online"}'
 while true
 do
 	temp=$(sensors -Au | grep temp2_input | sed 's/temp2_input://' | sed 's/   //')
 	mes=$(echo '{"Temp":{"actual":{"value":'$temp'},"metadata":{"type":"Updated"}}}')
-	mosquitto_pub -h 192.168.99.1 -t "$hw/events/device/$id/twin/update"
+	mosquitto_pub -h $ip -t "$hw/events/device/$id/twin/update"
 	sleep 10s
 done
