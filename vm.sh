f037e016f482a5b6070da5313f6cd81fff649eb2
Jan Unterbrink <jan.unterbrink@subpath.de>
2019-05-17 08:43:57 +0200

    using integers as timestamp instead of using strings

--- vm.sh
+++ vm.sh
@@ -21,7 +21,7 @@ while true
 do
 	#temp=$(sensors -Au | grep temp2_input | sed 's/temp2_input://' | sed 's/   //')
 	temp=$RANDOM
-	mes=$(echo '{"event_id":"","timestamp":"0","Twin":{"Temp":{"actual":{"value":'$temp'},"metadata":{"type":"Updated"}}}}')
+	mes=$(echo '{"event_id":"","timestamp":0,"Twin":{"Temp":{"actual":{"value":'$temp'},"metadata":{"type":"Updated"}}}}')
 	mosquitto_pub -q 0 -h $ip -t "\$hw/events/device/$id/twin/update" -m $mes
 	sleep 10s
 done
