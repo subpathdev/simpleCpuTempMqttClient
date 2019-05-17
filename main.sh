fcaa9d312f51da84236ad5094f9893337c329b39
Jan Unterbrink <jan.unterbrink@subpath.de>
2019-05-17 08:35:08 +0200

    adding Twin in JSON

--- main.sh
+++ main.sh
@@ -16,11 +16,12 @@ id=$1
 ip=$2
 
 
-mosquitto_pub -h $ip -t "$hw/events/device/$id/state/update" -m '{"state":"online"}'
+mosquitto_pub -q 0 -h $ip -t "\$hw/events/device/$id/state/update" -m '{"state":"online"}'
 while true
 do
 	temp=$(sensors -Au | grep temp2_input | sed 's/temp2_input://' | sed 's/   //')
-	mes=$(echo '{"Temp":{"actual":{"value":'$temp'},"metadata":{"type":"Updated"}}}')
-	mosquitto_pub -h $ip -t "\$hw/events/device/$id/twin/update" -m $mes
+
+	mes=$(echo '{"event_id":"","timestamp":"0","Twin":{"Temp":{"actual":{"value":'$temp'},"metadata":{"type":"Updated"}}}')
+	mosquitto_pub -q 0 -h $ip -t "\$hw/events/device/$id/twin/update" -m $mes
 	sleep 10s
 done
